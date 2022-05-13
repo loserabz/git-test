3d9b0781e76ffbce95b020d2aadb94c243b79764
fu <1518014551@qq.com>
2022-05-13 16:25:08 +0800

    第二次提交

--- src/main/java/com/fu/App.java
+++ src/main/java/com/fu/App.java
@@ -1,4 +1,9 @@
 package com.fu;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
---
+++ src/main/java/com/fu/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.fu;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("May You Rise As You Fall");
+    }
+}
